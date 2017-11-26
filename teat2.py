e855d6bff9dd89c1d4c32c3f0b79611a6cc26c17
xiaofei.ding <xiaofei.ding@qunar.com>
2017-11-27 00:23:40 +0800

    it is a true test2.py

--- teat2.py
+++ teat2.py
@@ -7,19 +7,27 @@ __author__ = xiaofei.ding
 公鸡5文钱一只，母鸡3文钱一只，小鸡3只一文钱，用100文钱买一百只鸡,其中公鸡，母鸡，小鸡都必须要有，
 问公鸡，母鸡，小鸡要买多少只刚好凑足100文钱。
 """
+import time
+start = time.clock()
 
 
 if __name__ == '__main__':
-    t = 1
-    x, y, z = 1, 1, 3*t
-    xx = 5*x
-    yy = 3*y
-    zz = t
+    # t = 1
+    # x, y, z = 1, 1, 1
+
     # i, j ,k = 1, 1, 1
-    while x>0 and y>0 and z>0:
-        if x+y+z==100 and xx+yy+zz==100:
-            print x,y,z
-    # i+=1
-    # j+=1
-    # k+=1
-    pass
+    for x in range(1, 20):
+        for y in range(1, 34):
+            for z in range(3, 100, 3):
+                xx = 5 * x
+                yy = 3 * y
+                zz = z / 3
+                if x + y + z == 100 and xx + yy + zz == 100:
+                    print x, y, z
+                    continue
+                else:
+                    pass
+                    # print 'no'
+                    # pass
+end = time.clock()
+print(end - start)
